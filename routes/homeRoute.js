53037135dbaa042ca2d1f18c9e2614d066346c6f
GabrielGoncalves0 <bie.gabriel1@outlook.com>
2024-03-26 19:55:20 -0300

    Feat: Layout - CSS

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -5,6 +5,6 @@ const homeControllers = require('../controllers/homeControllers');
 const router = express.Router();
 let control = new homeControllers();
 
-router.get('/', control.homeView());
+router.get('/', control.homeView);
 
 module.exports = router;
