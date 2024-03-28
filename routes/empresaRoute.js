22e233f93fb17f663dfbc1634e19ce7d0cc64691
Rodrigo oliveira <craftland262@gmail.com>
2024-03-28 00:04:15 -0300

    Revert "Feat: Correções CRUD - Empresas"
    This reverts commit 0dd8379bd2df80e1248a3b4264c3ac2068e5f5de.

--- routes/empresaRoute.js
+++ routes/empresaRoute.js
@@ -8,8 +8,8 @@ let control = new empresaControllers();
 router.get("/", control.listagemView);
 router.get("/cadastrar", control.cadastroView);
 router.post("/cadastrar", control.cadastrar);
-router.get("/alterar/:id", control.alterarView);
-router.post("/alterar", control.alterar);
+router.get("/alterar/:id", ctrl.alterarView);
+router.post("/alterar", ctrl.alterar);
 router.post("/excluir", control.excluir);
 
 module.exports = router;
